e8f13ebf466d803da969de69626ee1e4c61eff1b
maiaraltlawi  git config --global user.name maiaraltlawi <you@example.com>
2022-10-15 00:11:58 +0300

    hellowwww

---
+++ pand/1.js
@@ -0,0 +1,9 @@
+ var x={}
+ x.colomb="soso"
+ x.hary="gg"
+ console.log(x)
+"ho-ho-ho".split("-"); // ['ho', 'ho', 'ho']"ho-ho-ho".split("-"); // ['ho', 'ho', 'ho']
+var y = Math.random();
+console.log(y)
+var n =Math.ceil(0.51)
+console.log(n)
